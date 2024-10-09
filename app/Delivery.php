47c282bbce40d68130f1d6fad97bc86be4a45a6d
Kiibo-Groups <kiibogroups@gmail.com>
2024-10-08 20:17:52 -0700

    signup server nodejs

--- app/Delivery.php
+++ app/Delivery.php
@@ -96,12 +96,13 @@ class Delivery extends Authenticatable
                 'status'        => $add->status,
                 'status_admin'  => $add->status_admin,
             );
-            
-            if ($type == 'add') {
-                $req_ext = $addServer->newStaffDelivery($return);
-            }else {
-                $req_ext = $addServer->updateStaffDelivery($return);
-            }
+          
+            $req_ext = $addServer->newStaffDelivery($return);
+            // if ($type == 'add') {
+            //     $req_ext = $addServer->newStaffDelivery($return);
+            // }else {
+            //     $req_ext = $addServer->updateStaffDelivery($return);
+            // }
 
             if ($from == 'app') {
                 return ['msg' => 'done','user_id' => $add->id, 'external_id' => $add->external_id, 'req' => $req_ext]; 
